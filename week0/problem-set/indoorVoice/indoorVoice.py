9a75bba6113ea4ba756c218dc49266cad1230e82
Fernando Jr. Tria <161590347+FernandoJrTria@users.noreply.github.com>
2026-01-03 17:50:51 +0800

    Add initial files

---
+++ week0/problem-set/indoorVoice/indoorVoice.py
@@ -0,0 +1,8 @@
+def main():
+    # take user input in uppercase
+    phrase = input()
+
+    # print user input in lowercase
+    print(phrase.lower())
+
+main()
---
+++ week0/problem-set/playbackSpeed/playbackSpeed.py
@@ -0,0 +1,10 @@
+def main():
+    # take user input
+    # strip whitespace form left and right
+    phrase = input().strip()
+
+    # replace " " with ...
+    # print output
+    print(phrase.replace(" ", "..."))
+
+main()
